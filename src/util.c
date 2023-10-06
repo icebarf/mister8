b442a77a9456e184772f5042aa83c04268c97c8c
icebarf <sysgrammer@protonmail.com>
2023-10-06 21:52:04 +0530

    some utils for opcodes and stack

--- src/util.c
+++ src/util.c
@@ -5,12 +5,13 @@
 #include "util.h"
 
 /* utility cast macro */
-#define TO_ULONG(X) (unsigned long)X
+#define $to_ulong(X) (unsigned long)X
 
 static inline bool
 memory_available_for(unsigned long fsize)
 {
-  unsigned long available_space = MEMORY_CAP - MEMORY_RESERVED;
+  unsigned long available_space =
+    MEMORY_CAP - BYTES_RESERVED_LOW - BYTES_RESERVED_HIGH;
   return (fsize <= available_space) ? true : false;
 }
 
@@ -39,14 +40,14 @@ read_file(const char fname[static 1], memory_t dest_memory[static 1])
       break;
   }
 
-  if (!memory_available_for(TO_ULONG(fsize))) {
+  if (!memory_available_for($to_ulong(fsize))) {
     fprintf(stderr, "mister8: rom file too large to store\n");
     return -1;
   }
 
   size_t read_bytes =
-    fread(&(*dest_memory)[PROG_LOAD_ADDRESS], 1, TO_ULONG(fsize), rom);
-  if (read_bytes != TO_ULONG(fsize)) {
+    fread(&(*dest_memory)[PROG_LOAD_ADDRESS], 1, $to_ulong(fsize), rom);
+  if (read_bytes != $to_ulong(fsize)) {
     perror(fname);
     return -1;
   }
@@ -56,4 +57,28 @@ read_file(const char fname[static 1], memory_t dest_memory[static 1])
   /* we know that the read_bytes value is none larger than 0xe000 bytes, or
    * 3584 bytes and is non-negative */
   return (int)read_bytes;
+}
+
+/* 0xABCD
+ * pos = 1: D
+ * pos = 2: C
+ * pos = 3: B
+ * pos = 4: A
+ */
+uint8_t
+nibble(uint8_t pos, uint16_t number)
+{
+  switch (pos) {
+    case 1:
+      return number & 0x000f;
+    case 2:
+      return (number & 0x00f0) >> 4;
+    case 3:
+      return (number & 0x0f00) >> 8;
+    case 4:
+      return (number & 0xf000) >> 12;
+    default:
+      __builtin_unreachable();
+      break;
+  }
 }
--- src/util.h
+++ src/util.h
@@ -4,10 +4,43 @@
 #define MISTER8_DONT_WANT_SYSTEM
 #include "chip8.h"
 
-/* we want the type information to be retained for the memory parameter */
+/* we want the type information to be retained for a few parameters
+ * typedef is an easy way to not make declarations look messy*/
 typedef uint8_t(memory_t)[MEMORY_CAP];
+typedef uint8_t(display_t)[DISPLAY_W * DISPLAY_H];
+typedef uint16_t(stack_t)[STACK_SIZE];
+typedef uint8_t(registers_t)[REGISTER_COUNT];
 
 int
 read_file(const char fname[static 1], memory_t dest_memory[static 1]);
+
+/* opcode utils */
+
+/* 0xABCD
+ * pos = 1: D
+ * pos = 2: C
+ * pos = 3: B
+ * pos = 4: A
+ */
+uint8_t
+nibble(uint8_t pos, uint16_t number);
+
+/* 0xABCD
+ * low_byte: 0xCD
+ * high_byte: 0xAB
+ */
+#define $low_byte(X) (X & 0x00ff)
+
+#define $high_byte(X) ((X & 0xff00) >> 8)
+
+/* 0xABCD
+ * high byte's first nibble and low byte combined
+ */
+#define $high_nib_low_byte(X) (X & 0x0fff)
+
+/* stack utils */
+#define $pop(stack_p, stack_cntr_p) (*stack)[(*stack_cntr_p)--]
+
+#define $push(stack_p, stack_cntr_p, val) ((*stack)[++(*stack_cntr_p)] = val)
 
 #endif
